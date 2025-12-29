11c4f4cb09482a9564785453a53dd9b3e85f1f9e
Karina Azzahra <krinazzhra@gmail.com>
2025-12-30 02:48:29 +0700

    twinkle twinkle little star, no i am a big star wleeee

--- src/main/java/com/minari/ecommerce/repository/ProductReviewRepository.java
+++ src/main/java/com/minari/ecommerce/repository/ProductReviewRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @Repository
 public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
+    @org.springframework.data.jpa.repository.EntityGraph(attributePaths = {"customer"})
     List<ProductReview> findByProductId(Long productId);
 
     List<ProductReview> findByCustomerId(Long customerId);
